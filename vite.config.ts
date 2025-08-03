a9686126a6801cc744acff975839a007f9a09cc3
Pakakothaproject <pakakothaproject@gmail.com>
2025-08-03 10:53:30 +0600

    Add files via upload

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,11 @@
 import path from 'path';
 import { defineConfig, loadEnv } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
     return {
+      plugins: [react()],
       define: {
         // As per @google/genai guidelines, the API key must be sourced from process.env.API_KEY.
         'process.env.API_KEY': JSON.stringify(env.API_KEY),
