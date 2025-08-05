492435d9bf3a2b801446331630c1144a632b0ce1
Pakakothaproject <pakakothaproject@gmail.com>
2025-08-05 06:54:47 +0600

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -9,6 +9,7 @@ export default defineConfig(({ mode }) => {
       define: {
         // As per @google/genai guidelines, the API key must be sourced from process.env.API_KEY.
         'process.env.API_KEY': JSON.stringify(env.API_KEY),
+        'process.env.API_KEY2': JSON.stringify(env.API_KEY2),
         'process.env.NEWSDATA_API_KEY': JSON.stringify(env.NEWSDATA_API_KEY)
       },
       resolve: {
